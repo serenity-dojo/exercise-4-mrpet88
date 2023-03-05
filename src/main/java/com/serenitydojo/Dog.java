9f26a96b05dcc9bcbc7994969148d0150df2640f
Marinos Petratos <petratos88@gmail.com>
2023-03-05 18:39:57 +0100

    add Dog class

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,24 @@
+package com.serenitydojo;
+
+public class Dog{
+    private String name;
+    private String favoriteToy;
+    private int age;
+    private boolean isFed;
+
+    public static final String DOG_NOISE = "Woof";
+
+public Dog(String name, String favoriteToy, int age){
+    this.name = name;
+    this.favoriteToy = favoriteToy;
+    this.age = age;
+}
+
+public String makeNoise(){
+    return DOG_NOISE;
+}
+public void feed(){
+    this.isFed=true;
+}
+
+}
